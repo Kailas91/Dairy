13871c11f235fb9db417de56840859c8a42ac089
kailas91 <klandge82@gmail.com>
2019-09-14 14:56:27 +0530

    ok

--- new kailas/html/day8/ass1/demo_workers.js
+++
@@ -1,9 +0,0 @@
-
-
-function timedCount() {
-  postMessage(new Date().toLocaleTimeString());
-  //return the current i value
-  setTimeout("timedCount()",10);
-  //set maximun timeout limit
-}
-timedCount();
